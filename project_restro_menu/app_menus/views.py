890938882342a0bd49aa5d126371a976dba9038d
ashusza <asijapati58@gmail.com>
2023-06-03 20:23:59 +0545

    templates add in it

--- project_restro_menu/app_menus/views.py
+++ project_restro_menu/app_menus/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def list_menu(request):
+    return render(request,'menus/list_menu.html')
+
+def add_menu(request):
+    return render(request,'menus/add_menu.html')
--- project_restro_menu/project_restro_menu/urls.py
+++ project_restro_menu/project_restro_menu/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project_restro_menu project.
+URL configuration for project_cafe_menu project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_menus import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('menus/', views.list_menu , name='menu-list'),
+    path('menus/', views.add_menu, name='menu-add')
 ]
